b8e16dd6614e2b52640281207a9fd15eb1bb524e
charly12543 <acuario12543@gmail.com>
2025-06-23 17:09:16 -0600

    Actualizacion POM principál del proyecto

--- ventas/src/main/java/com/charlyCorporation/ventas/controller/VentasController.java
+++ ventas/src/main/java/com/charlyCorporation/ventas/controller/VentasController.java
@@ -5,7 +5,6 @@ import com.charlyCorporation.ventas.model.Ventas;
 import com.charlyCorporation.ventas.service.IVentasService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
